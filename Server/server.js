ff2d74923b4dae030cd3d5460f4fde8fe50a9a52
shrikant upadhyay <shrikantupadhyay@Shrikants-MacBook-Air.local>
2018-02-27 23:59:16 +0530

    Setup start script

--- Server/server.js
+++ Server/server.js
@@ -1,12 +1,13 @@
 // Built in module Does not require to init
 const path = require('path');
 const express = require('express');
+const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
 
 var app = express();
 
 app.use(express.static(publicPath));
-const port = process.env.PORT || 3000;
+
 app.listen(port,()=>{
   console.log(`Server is up on port ${port}` );
 });
